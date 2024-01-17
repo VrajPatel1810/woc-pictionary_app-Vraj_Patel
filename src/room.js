a2f4d6c837a4e21eec79ddbe36c2727aa02c1a47
VrajPatel1810 <202101060@daiict.ac.in>
2024-01-17 18:39:45 +0530

    Frontend complete

--- src/room.js
+++ src/room.js
@@ -1,13 +1,13 @@
 import { Container, Input, Typography, createTheme } from '@mui/material/';
 import './App.css';
-//import { Container } from '@mui/material';
 import { green } from '@mui/material/colors';
 import { ThemeProvider } from '@mui/material/';
 import * as React from 'react';
 //import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import { Link as LINK } from 'react-router-dom';
+import { Link as LINK } from 'react-router-dom'
+
 
 const theme = createTheme({
     palette: {
@@ -20,8 +20,8 @@ function Room() {
     return (
         <ThemeProvider theme={theme}>
             <Container style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', maxWidth: "1500px" }} >
-                <Container style={{ display: 'flex', flex: '2', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '55px', border: '2px solid green' }} >
-                    <Container style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '25px', border: '2px solid blue' }} >
+                <Container style={{ display: 'flex', flex: '2', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '20px', border: '2px solid black', borderRadius: '10px', height: '650px', marginRight: '10px' }} >
+                    <Container style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '25px', border: '2px solid black', borderRadius: '10px', height: '400px' }} >
                         <Typography>User 1</Typography>
                     </Container>
                     <Container style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '25px' }} >
@@ -30,20 +30,28 @@ function Room() {
                         <Button variant="contained" style={{ backgroundColor: 'rgb(42, 206, 53)', height: '40px', marginTop: '25px', marginBottom: '25px', width: '100%' }} >Leave</Button>
                     </Container>
                 </Container>
-                <Container style={{ display: 'flex', flex: '5', justifyContent: 'center', alignItems: 'center', marginTop: '55px', border: '2px solid red' }} >
-                    <Container style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '25px', border: '2px solid blue' }} >
+                <Container style={{ display: 'flex', flex: '5', justifyContent: 'center', alignItems: 'center', marginTop: '20px', border: '2px solid black', borderRadius: '10px', height: '650px', marginRight: '10px' }} >
+                    <Container style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '25px', border: '2px solid black', borderRadius: '10px', height: '500px' }} >
                         <Typography>Chat</Typography>
                     </Container>
                 </Container>
-                <Container style={{ display: 'flex', flex: '3', justifyContent: 'center', alignItems: 'center', marginTop: '55px', border: '2px solid yellow' }} >
-                    <Container style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '25px', border: '2px solid blue' }} >
-                        <Container style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: '25px' }} >
-                            <Input placeholder="Type a Message" style={{ width: '100%' }} />
+                <Container style={{ display: 'flex', flex: '3', justifyContent: 'center', alignItems: 'center', marginTop: '20px', border: '2px solid black', borderRadius: '10px', height: '650px' }} >
+                    <Container style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '25px', border: '2px solid black', borderRadius: '10px', height: '600px' }} >
+                        <Container style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: '25px', marginTop: 'auto', marginBottom: '25px', width: '350px' }} >
+                            <TextField
+                                id="outlined-multiline-flexible"
+                                label="Message"
+                                placeholder='Type your message here'
+                                multiline
+                                maxRows={4}
+                                style={{ width: '100%' }}
+                            />
+                            <Button variant="contained" style={{ backgroundColor: 'rgb(42, 206, 53)', width: '25%', marginLeft: '3px', height: '55px' }} >SEND</Button>
                         </Container>
                     </Container>
                 </Container>
             </Container>
-        </ThemeProvider>
+        </ThemeProvider >
     );
 }
 
